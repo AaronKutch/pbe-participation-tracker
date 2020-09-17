fd660a9b1757d13d879a26ec3c6ea639414c48fc
Cameron Brock <cameronbrock@tamu.edu>
2020-09-16 21:33:40 -0500

    Undid many recent changes which I had difficulty fixing (I apologize for the excessive number of commits).

--- app/controllers/db_test_controller.rb
+++
@@ -1,4 +0,0 @@
-class DbTestController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  root 'db_test#index'
-  get 'db_test/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
