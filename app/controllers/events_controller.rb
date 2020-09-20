8cebcba5f4fb9b001e24c9f8205268427785e24a
Cameron Brock <cameronbrock@tamu.edu>
2020-09-20 15:28:20 -0500

    Added some input checking on the client and server sides to ensure that the user is providing all necessary information.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,41 +1,70 @@
 class EventsController < ApplicationController
+
+	@time_zone = 6
+
   def index
 	@events = Event.order("date")
   end
 
   def show
-	@event_record = Event.find(params[:id])
+  	begin
+		@event_record = Event.find(params[:id])
+	rescue
+		redirect_to events_path
+	end
   end
 
   def new
 	@new_event = Event.new
-	@time = ""
   end
 
   def create
-	@event_info = params["event"]	
-	Event.create(:title => @event_info["title"], :description => @event_info["description"], :date => @event_info["date"], :location => @event_info["location"], :mandatory => @event_info["mandatory"])
-	redirect_to events_path
+	begin
+		@event_info = params["event"]
+		puts "Date — " + @event_info["date"].to_s
+		dt = DateTime.parse(@event_info["date(1i)"].to_s + "-" + @event_info["date(2i)"].to_s + "-" + @event_info["date(3i)"].to_s + "T" + @event_info["date(4i)"].to_s + ":" + @event_info["date(5i)"].to_s + "+0" + @time_zone.to_s + ":00")
+		Event.create(:title => @event_info["title"], :description => @event_info["description"], :date => dt, :location => @event_info["location"], :mandatory => @event_info["mandatory"])
+		redirect_to events_path
+	rescue
+		redirect_to new_event_path
+	end
   end
 
   def edit
-  	@event = Event.find(params[:id])
+  	begin
+  		@event = Event.find(params[:id])
+  	rescue
+  		redirect_to events_path
+  	end
   end
 
   def update
-  	@event_info = params["event"]
-  	@event = Event.find(params[:id])
-  	@event.update_attributes(:title => @event_info["title"], :description => @event_info["description"], :date => @event_info["date"], :location => @event_info["location"], :mandatory => @event_info["mandatory"])
-  	redirect_to events_path
+  	begin
+  		 @event_info = params["event"]
+  		 @event = Event.find(params[:id])
+  		 puts "DATE --  " + @event_info["date"].to_s
+  		 dt = DateTime.parse(@event_info["date(1i)"].to_s + "-" + @event_info["date(2i)"].to_s + "-" + @event_info["date(3i)"].to_s + "T" + @event_info["date(4i)"].to_s + ":" + @event_info["date(5i)"].to_s + "+0" + @time_zone.to_s + ":00")
+  		 @event.update_attributes(:title => @event_info["title"], :description => @event_info["description"], :date => dt, :location => @event_info["location"], :mandatory => @event_info["mandatory"])
+  		redirect_to events_path
+  	rescue
+  		redirect_to edit_event_path
+  	end
   end
 
   def delete
-	@event_record = Event.find(params[:id])
+    begin
+    	@event_record = Event.find(params[:id])
+    rescue
+		redirect_to events_path
+	end
   end
 
   def destroy
-	@event_record = Event.find(params[:id])
-	@event_record.destroy
+  	begin
+		@event_record = Event.find(params[:id])
+		@event_record.destroy
+	rescue
+	end
 	redirect_to events_path
   end
 end
