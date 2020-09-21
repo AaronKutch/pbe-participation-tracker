78e46ad8c49e25c3c2ff1b29237e65b0451928bc
Sergio Rios <SDRiosPerez@outlook.com>
2020-09-21 10:14:27 -0500

    Initial views impl; Generated boilerplate for all the necessary views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  root 'login/index'
+  get 'users/index'
+  get 'users/create'
+  get 'users/update'
+  get 'users/dashboard'
+  get 'events/index'
+  get 'events/create'
+  get 'events/update'
+  get 'events/dashboard'
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/requests/events_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Events", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/events/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/events/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/events/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /dashboard" do
+    it "returns http success" do
+      get "/events/dashboard"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
