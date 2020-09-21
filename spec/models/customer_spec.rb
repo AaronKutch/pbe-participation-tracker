c8b1cc7dfd3eb2fa184ae33110594f71350be793
cobraguy <cobraguy@users.noreply.github.com>
2020-09-21 00:31:16 -0500

    Add tests for customer model

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  describe 'constructor' do
+    context 'when created with no arguments' do
+      it 'is not valid' do
+        expect(Customer.new.valid?).to eq(false)
+      end
+    end
+
+    context 'when created with only a name' do
+      it 'is not valid' do
+        expect(Customer.new(:first_name => "John", :last_name => "Doe").valid?).to eq(false)
+      end
+    end
+
+    context 'when created with name, email, and password' do
+      it 'is valid' do
+        expect(Customer.new(:first_name => "John", :last_name => "Doe", :email => "johndoe@mail.com", :password => "password").valid?).to eq(true)
+      end
+    end
+
+    context 'when created' do
+        it 'role is "user" by default' do
+            expect(Customer.new.role).to eq("user")
+        end
+    end
+  end
+end
