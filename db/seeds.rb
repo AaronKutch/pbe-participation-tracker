95fbed69104276425605d0ce23fa5b80bca480f2
AaronKutch <aaronkutch@att.net>
2020-09-22 17:48:50 -0500

    improve seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,39 @@
-# NOTE this seeding is only for manual testing purposes for development
-puts "Seeding a database from `seeds.rb`, only do this on the development database."
-c = Customer.new
-c.first_name = 'f'
-c.last_name = 'l'
-c.email = 'admin@tamu.edu'
-c.role = 'admin'
-c.password = 'p'
-if !c.save
-    # best solved by `rake db:reset db:migrate db:seed`
-    puts "Error: could not add admin test account"
-end
+# NOTE this seeding is only for manual testing purposes for development.
+# Can be reproduced with `rake db:reset db:migrate db:seed`.
+if Rails.env.development?
+  e = Event.new
+  e.title = 'test event'
+  e.description = 'this is a test event for development purposes'
+  e.date = 'January 2021'
+  e.location = 'Texas A&M College Station'
+  e.mandatory = false
+  unless e.save
+    puts "Error: could not add test event"
+  end
 
-c = Customer.new
-c.first_name = 'f'
-c.last_name = 'l'
-c.email = 'guest@tamu.edu'
-c.role = 'guest'
-c.password = 'p'
-if !c.save
+  c0 = Customer.new
+  c0.first_name = 'test'
+  c0.last_name = 'admin'
+  c0.email = 'admin@tamu.edu'
+  c0.role = 'admin'
+  c0.password = 'p'
+  if c0.save
+    c0.events << e
+  else
+    puts "Error: could not add admin test account"
+  end
+
+  c1 = Customer.new
+  c1.first_name = 'test'
+  c1.last_name = 'guest'
+  c1.email = 'guest@tamu.edu'
+  c1.role = 'guest'
+  c1.password = 'p'
+  if c1.save
+    c1.events << e
+  else
     puts "Error: could not add guest test account"
+  end
+else
+  puts "Error: tried to run `seeds.rb` outside of development environment"
 end
