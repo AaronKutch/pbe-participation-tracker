3422474530dc3ac8b23b38cc4ca0e0ba1a9483f6
Cameron Brock <cameronbrock@tamu.edu>
2020-10-02 01:29:39 -0500

    Added delete functionality for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,25 @@ class UsersController < ApplicationController
     @user = Customer.find(params[:id])
     @user_events = @user.events.order('date')
   end
+  
+  def edit
+  end
+  
+  def update
+  end
 
   def create; end
 
-  def update; end
-
   def dashboard; end
+  
+  def delete
+    @user = Customer.find(params[:id])
+  end
+  
+  def destroy
+    @user = Customer.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+  
 end
