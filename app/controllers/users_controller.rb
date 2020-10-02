ec10b07934ab8a93077f537b20e1804cb897cf99
Cameron Brock <cameronbrock@tamu.edu>
2020-10-01 22:26:49 -0500

    Added functionality for viewing the list of current users, as well as viewing information for each user including which events they have attended.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = Customer.order('last_name')
+  end
+  
+  def show
+    @user = Customer.find(params[:id])
+    @user_events = @user.events.order('date')
+  end
 
   def create; end
 
