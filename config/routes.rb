4c8a3600b8a859c52219ce7480ffafd5caf37476
Sergio Rios <SDRiosPerez@outlook.com>
2020-10-07 18:22:10 -0500

    added conditional rendering and styling to member views, and my profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,11 @@
 Rails.application.routes.draw do
   root 'access#login'
   # Routes will be used in sprint 2
-  # get 'users/index'
-  # get 'users/create'
-  # get 'users/update'
-  # get 'users/dashboard'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root 'events#index'
-
   get 'access/login'
-  post 'access/attempt_login'
   get 'access/logout'
   get 'access/new_account'
   post 'access/create_account'
+  post 'access/attempt_login'
 
   post 'events/mark_attendance'
   resources :events do
