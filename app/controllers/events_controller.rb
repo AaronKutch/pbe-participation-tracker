749b051d8571bc4afacf7b37369ad2b770cf77fb
Cameron Brock <cameronbrock@tamu.edu>
2020-10-09 10:34:04 -0500

    Changes to Events controller to prevent errors.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,17 +25,21 @@ class EventsController < ApplicationController
 
   def show
     begin
-      @event_record = Event.find(params[:id])
+      @event_record = Event.find_by(id: params[:id])
+      if (@event_record == nil)
+        flash[:notice] = 'This event does not exist.'
+        raise StandardError.new 'error'
+      end
     rescue StandardError
-      redirect_to events_path
+      return redirect_to(events_path)
     end
-
     @user_role = session[:user_id] ? Customer.where(id: session[:user_id]).first.role : 'not_logged_in'
     Time.use_zone('Central Time (US & Canada)') do
       @utc_offset = Time.zone.parse(Date.current.to_s).dst? ? 5.hours : 6.hours
     end
 
     @attendees = @user_role == 'admin' ? @event_record.customers : []
+
   end
 
   def new
@@ -47,14 +51,17 @@ class EventsController < ApplicationController
     Event.create(title: @event_info['title'], description: @event_info['description'], date: construct_date_time,
                  end_time: construct_end_time, location: @event_info['location'], mandatory: @event_info['mandatory'])
     redirect_to events_path
-  rescue StandardError
+  rescue
     redirect_to new_event_path
   end
 
   def edit
-    @event = Event.find(params[:id])
+    @event = Event.find_by(id: params[:id])
+    if (@event == nil)
+      raise StandardError.new 'error'
+    end
   rescue StandardError
-    redirect_to events_path
+    redirect_to(events_path)
   end
 
   def update
@@ -68,15 +75,25 @@ class EventsController < ApplicationController
   end
 
   def delete
-    @event_record = Event.find(params[:id])
+    @event_record = Event.find_by(id: params[:id])
+    if (@event_record == nil)
+      raise StandardError.new 'error'
+    end
   rescue StandardError
     redirect_to events_path
   end
 
   def destroy
-    @event_record = Event.find(params[:id])
-    @event_record.destroy
-    redirect_to events_path
+    begin
+      @event_record = Event.find_by(id: params[:id])
+      if (@event_record == nil)
+        raise StandardError.new 'error'
+      end
+      @event_record.destroy
+      redirect_to(events_path)
+    rescue StandardError
+      redirect_to(events_path)
+    end
   end
 
   def mark_attendance
