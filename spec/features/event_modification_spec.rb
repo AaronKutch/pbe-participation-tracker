43957a81edcb9f941f90e8a29e66ff5f5c3d40a7
Cameron Brock <cameronbrock@tamu.edu>
2020-10-09 10:35:08 -0500

    Added more tests for Events modification (events controller now at 95.89% test coverage).

--- spec/features/event_modification_spec.rb
+++ spec/features/event_modification_spec.rb
@@ -3,6 +3,15 @@
 require 'rails_helper'
 require_relative '../common'
 
+# Attempt to access events index without logging in.
+RSpec.describe 'Attempt to access events index without logging in.', js: true do
+  it 'Redirects the user back to login page.' do
+    visit('/')
+    visit('/events/1/edit')
+    expect(current_path).to eql('/access/login')
+  end
+end
+
 # Make sure that the new event page is visitable.
 RSpec.describe 'Visit the new event page' do
   it 'Goes to the new event page.' do
@@ -35,6 +44,27 @@ RSpec.describe 'Create a new event.' do
   end
 end
 
+# Attempt to create an event with a title that is more
+# than 50 characters long.
+RSpec.describe 'Create a new event with a title that is too long.' do
+  it 'Redirects the user to /events page.' do
+    admin_create_and_login
+    
+    # Create new event.
+    click_on('Add new event')
+    @x = ''
+    for i in 0..100 do
+      @x += i.to_s
+    end
+    fill_in('event_title', with: @x)
+    fill_in('event_location', with: @x)
+    click_on('Submit')
+    expect(current_path).to eql('/events')
+    expect(page).to have_no_content(@x)
+    
+  end
+end
+
 # Successfully edit event.
 RSpec.describe 'Edit an event.' do
   it 'Changes the name of the created event.' do
@@ -57,6 +87,70 @@ RSpec.describe 'Edit an event.' do
 
     expect(current_path).to eql('/events')
     expect(page).to have_content('EDITED EVENT TITLE')
+  end
+end
+
+# Attempt to edit an event that does not exist.
+RSpec.describe 'Edit a non-existant event.' do
+  it 'Redirects user back to /events page.' do
+    admin_create_and_login
+    
+    # Attempt to edit event when none has been created.
+    visit('/events/500/edit')
+    expect(current_path).to eql('/events')
+  end
+end
+
+# Attempt to edit an event without admin permissions.
+RSpec.describe 'Edit a page without admin permissions.' do
+  it 'Redirects user back to /users path.' do
+    @c = Customer.create(first_name: 'Jane', last_name: 'Doe', email: 'js@email.com', role: 'user', password: 'p')
+    common_login('js@email.com', 'p')
+    visit('/users/' + @c.id.to_s + '/edit')
+    expect(page).to have_content('You do not have admin permissions.')
+    
+  end
+end
+
+# Attempt to delete an event without admin permissions.
+RSpec.describe 'Delete a page without admin permissions.' do
+  it 'Redirects user back to /users path.' do
+    @c = Customer.create(first_name: 'Jane', last_name: 'Doe', email: 'js@email.com', role: 'user', password: 'p')
+    common_login('js@email.com', 'p')
+    visit('/users/' + @c.id.to_s + '/delete')
+    expect(page).to have_content('You do not have admin permissions.')
+    
+  end
+end
+
+# Attempt to edit an event to have a title that is
+# more than 50 characters long.
+RSpec.describe 'Edit an event to have a title that is too long.' do
+  it 'Redirects user back to /events path.' do
+    admin_create_and_login
+    
+    # Setting up event:
+    click_on('Add new event')
+    fill_in('event_title', with: 'TEST EVENT ONE')
+    fill_in('event_location', with: 'TEST LOCATION ONE')
+    click_on('Submit')
+
+    expect(current_path).to eql('/events')
+    expect(page).to have_content('TEST EVENT ONE')
+
+    # Editing:
+    all('a', text: 'Edit')[0].click
+    expect(page).to have_content('Title')
+    @x = ''
+    for i in 0..100 do
+      @x += i.to_s
+    end
+    fill_in('event_title', with: @x)
+    click_on('Submit')
+
+    expect(current_path).to eql('/events')
+    expect(page).to have_content('TEST EVENT ONE')
+    expect(page).to have_no_content(@x)
   end
 end
 
@@ -123,6 +217,46 @@ RSpec.describe 'Delete an event.' do
   end
 end
 
+# Attempt to delete an event that has already been deleted.
+RSpec.describe 'Delete an event that has already been deleted.' do
+  it 'Redirects the user back to the /events path.' do
+    admin_create_and_login
+    
+    # Create the event.
+    visit('/events/new')
+    fill_in('event_title', with: 'TEST EVENT ONE')
+    fill_in('event_location', with: 'TEST LOCATION ONE')
+    click_on('Submit')
+
+    expect(current_path).to eql('/events')
+    expect(page).to have_content('TEST EVENT ONE')
+
+    # Delete the event.
+    all('a', text: 'Delete')[0].click
+    expect(current_path).to include('delete')
+    
+    # Destroy the event from database.
+    @e = Event.first
+    @e.destroy
+    
+    # Attempt to delete the event again.
+    click_on('Delete Event')
+    expect(current_path).to eql('/events')
+    
+  end
+end
+
+# Attempt to delete an event that does not exist.
+RSpec.describe 'Delete an event that does not exist.' do
+  it 'Redirects user back to /events path.' do
+    admin_create_and_login
+    
+    # Attempt to access delete page that does not exist.
+    visit('/events/500/delete')
+    expect(current_path).to eql('/events')
+  end
+end
+
 # Details an event.
 RSpec.describe 'Details an event.' do
   it 'Displays event details.' do
@@ -143,6 +277,19 @@ RSpec.describe 'Details an event.' do
 
     expect(page).to have_content('Location')
     expect(page).to have_content('TEST LOCATION ONE')
+  end
+end
+
+# Details of an event which no longer exists.
+RSpec.describe 'Details of a non-existant event.' do
+  it 'Redirects user back to /events page.' do
+  
+    admin_create_and_login
+    
+    # Attempt to access details of an event before creating one.
+    visit('/events/1')
+    expect(current_path).to eql('/events')
+  
   end
 end
 
@@ -204,39 +351,101 @@ end
 
 # Register for a new event, at which point the sign-in for that
 # event will no longer be visible.
-# RSpec.describe 'Register for a new event.' do
-#  it 'Removes the sign-in button from view.' do
-#    admin_create_and_login()
-#
-#    user_email = 'user@test.com'
-#    user_password = 'p'
-#    Customer.create(
-#      :first_name => 'Jane',
-#      :last_name => 'Doe',
-#      :role => 'user',
-#      :email => user_email,
-#      :password => user_password
-#    )
+RSpec.describe 'Register for a new event.' do
+ it 'Removes the sign-in button from view.' do
+   admin_create_and_login()
+   user_email = 'user@test.com'
+   user_password = 'p'
+   Customer.create(
+     :first_name => 'Jane',
+     :last_name => 'Doe',
+     :role => 'user',
+     :email => user_email,
+     :password => user_password
+   )
 
-# Create an event.
-#    click_on('Add new event')
-#    expect(current_path).to eql('/events/new')
-#    fill_in('event_title', :with => 'Event #1')
-#    fill_in('event_location', :with => 'Location #1')
-#    click_on('Submit')
-#    expect(current_path).to eql('/events')
+   # Create an event.
+   click_on('Add new event')
+   expect(current_path).to eql('/events/new')
+   fill_in('event_title', :with => 'Event #1')
+   fill_in('event_location', :with => 'Location #1')
+   
+   select 'January', from: 'event_date_2i'
+   select '1', from: 'event_date_3i'
+   select '2020', from: 'event_date_1i'
+   select '12 AM', from: 'event_date_4i'
+   select '00', from: 'event_date_5i'
 
-# Log back in as a user.
-#    click_on('Logout')
-#    click_on('Login')
-#    common_login(user_email, user_password)
+   select 'December', from: 'event_end_time_2i'
+   select '31', from: 'event_end_time_3i'
+   select '2020', from: 'event_end_time_1i'
+   select '11 PM', from: 'event_end_time_4i'
+   select '59', from: 'event_end_time_5i'
+   
+   click_on('Submit')
+   expect(current_path).to eql('/events')
 
-# Sign into Event #1.
-#    all('a', :text => 'Sign In')[0].click
-#    visit('/events')
+   # Log back in as a user.
+   click_on('Logout')
+   common_login(user_email, user_password)
 
-#  end
-# end
+   # Sign into Event #1.
+   all('a', :text => 'Sign In')[0].click
+   visit('/events')
+
+ end
+end
+
+# Register for an event that has already been
+# registered for.
+RSpec.describe 'Register for an event again.' do
+ it 'Removes the sign-in button from view.' do
+   admin_create_and_login()
+   user_email = 'user@test.com'
+   user_password = 'p'
+   Customer.create(
+     :first_name => 'Jane',
+     :last_name => 'Doe',
+     :role => 'user',
+     :email => user_email,
+     :password => user_password
+   )
+
+   # Create an event.
+   click_on('Add new event')
+   expect(current_path).to eql('/events/new')
+   fill_in('event_title', :with => 'Event #1')
+   fill_in('event_location', :with => 'Location #1')
+   
+   select 'January', from: 'event_date_2i'
+   select '1', from: 'event_date_3i'
+   select '2020', from: 'event_date_1i'
+   select '12 AM', from: 'event_date_4i'
+   select '00', from: 'event_date_5i'
+
+   select 'December', from: 'event_end_time_2i'
+   select '31', from: 'event_end_time_3i'
+   select '2020', from: 'event_end_time_1i'
+   select '11 PM', from: 'event_end_time_4i'
+   select '59', from: 'event_end_time_5i'
+   
+   click_on('Submit')
+   expect(current_path).to eql('/events')
+
+   # Log back in as a user.
+   click_on('Logout')
+   common_login(user_email, user_password)
+
+   # Sign into Event #1.
+   all('a', :text => 'Sign In')[0].click
+   visit('/events')
+   
+   # Sign in again.
+   all('a', :text => 'Sign In')[0].click
+   expect(page).to have_content('You have already registered for this event.')
+
+ end
+end
 
 # Full test of modifying each field.
 RSpec.describe 'Create a new event with each field modified from defaults.' do
@@ -265,5 +474,69 @@ RSpec.describe 'Create a new event with each field modified from defaults.' do
     expect(page).to have_content("Jul 9, #{Date.today.year}, 7:00 pm") # Time
     expect(page).to have_content('Event location A') # Location
     expect(page).to have_content('false') # Mandatory
+  end
+end
+
+# Revoke attendance of a user.
+RSpec.describe 'Revoke attendance for a user.' do
+ it 'The user will no longer be signed in for that event.' do
+   admin_create_and_login()
+   user_email = 'user@test.com'
+   user_password = 'p'
+   Customer.create(
+     :first_name => 'Jane',
+     :last_name => 'Doe',
+     :role => 'user',
+     :email => user_email,
+     :password => user_password
+   )
+
+   # Create an event.
+   click_on('Add new event')
+   expect(current_path).to eql('/events/new')
+   fill_in('event_title', :with => 'Event #1')
+   fill_in('event_location', :with => 'Location #1')
+   
+   select 'January', from: 'event_date_2i'
+   select '1', from: 'event_date_3i'
+   select '2020', from: 'event_date_1i'
+   select '12 AM', from: 'event_date_4i'
+   select '00', from: 'event_date_5i'
+
+   select 'December', from: 'event_end_time_2i'
+   select '31', from: 'event_end_time_3i'
+   select '2020', from: 'event_end_time_1i'
+   select '11 PM', from: 'event_end_time_4i'
+   select '59', from: 'event_end_time_5i'
+   
+   click_on('Submit')
+   expect(current_path).to eql('/events')
+
+   # Log back in as a user.
+   click_on('Logout')
+   common_login(user_email, user_password)
+
+   # Sign into Event #1.
+   all('a', :text => 'Sign In')[0].click
+   visit('/events')
+   
+   # Log back out and login as admin.
+   click_on('Logout')
+   common_login('admin@test.com', 'p')
+   all('a', text: 'Details')[0].click
+   expect(page).to have_content('Jane Doe')
+   click_on('Revoke Sign In')
+   visit('/events')
+   
+   # Check if user is still signed in.
+   expect(page).to have_no_content('Jane Doe')
+
+ end
+end
+
+# Attempt to revoke attendance for a user that does not exist.
+RSpec.describe 'Revoke attendance for a non-existant user.' do
+  it 'Returns the user to the /events path.' do
+  
   end
 end
