99ef3ad70543b491abe0b04d49b52cf9913d5ba7
Shawn <shawntan2008@gmail.com>
2020-10-16 18:52:48 -0500

    fixed minor typo in events controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -102,7 +102,7 @@ class EventsController < ApplicationController
     @event.customers.delete(@user)
     redirect_to("/events/#{params[:event]}")
   rescue StandardError
-    flash[:notice] = 'Student has no signed in.'
+    flash[:notice] = 'Student has not signed in yet.'
   end
 
   private
