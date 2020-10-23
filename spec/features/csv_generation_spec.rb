eb16584d1ee5ca76fd72e2ae1dd84e1d48a47d90
cobraguy <cobraguy@users.noreply.github.com>
2020-10-23 00:54:05 -0500

    Improve code coverage

--- spec/features/csv_generation_spec.rb
+++ spec/features/csv_generation_spec.rb
@@ -8,7 +8,18 @@ RSpec.describe 'Attempt to generate a CSV of attendances' do
   it 'Returns the /users/export_attendance_csv page.' do
     admin_create_and_login
 
-    # Attempt to generate a QR code for event.
+    Event.create(
+      title: 'TEST EVENT',
+      description: 'This is an example event.',
+      date: '2020-01-01 00:00:00',
+      location: 'TEST LOCATION',
+      mandatory: true,
+      end_time: '2021-02-01 00:00:00'
+    )
+
+    Customer.first.events << Event.first
+
+    # Attempt to generate a CSV file.
     visit(users_path)
     click_on('Export Attendance CSV')
     expect(current_path).to eq(users_export_attendance_csv_path)
